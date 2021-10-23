57135447274b82f625aa170375d6c9af08239e21
Martin Burkert <chrimbu@ncmm.uio.no>
2021-10-23 08:55:04 +0200

    Upgrade to playwright 1.12.1 and fix date comparison issue in rebook.py

--- rebook.py
+++ rebook.py
@@ -1,11 +1,10 @@
 import tempfile
-from calendar import monthrange
-
 import dateparser
-import playwright.helper
-import playwright.sync_api
+import datetime
 import telegram_send
-from playwright import sync_playwright
+from calendar import monthrange
+from playwright.sync_api import sync_playwright,
+     Page as PlaywrightPage, TimeoutError as PlaywrightTimeoutError
 from pydantic import BaseSettings, SecretStr
 
 
@@ -19,7 +18,7 @@ class Settings(BaseSettings):
         env_file_encoding = "utf-8"
 
 
-def send_success(page: playwright.sync_api.Page, msg: str):
+def send_success(page: PlaywrightPage, msg: str):
     telegram_send.send(messages=[msg])
     with tempfile.TemporaryFile("r+b") as fp:
         encoded_img = page.screenshot(type="png")
@@ -33,7 +32,7 @@ def main():
 
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
-        page = browser.newPage()
+        page = browser.new_page()
         try:
             page.goto("https://selfservice.udi.no/en-gb/")
             # click on log in button
@@ -42,7 +41,7 @@ def main():
             page.type("input[type=email]", config.EMAIL)
             page.type("input[type=password]", config.PWD.get_secret_value())
             page.click("#next")
-        except playwright.helper.TimeoutError:
+        except PlaywrightTimeoutError:
             msg = "Seems like UDI website is down or you are offline"
             print(msg)
             telegram_send.send(messages=[msg])
@@ -50,11 +49,11 @@ def main():
 
         try:
             book_btn_id: str = "#ctl00_BodyRegion_PageRegion_MainRegion_IconNavigationTile2_heading"
-            page.waitForSelector(book_btn_id)
+            page.wait_for_selector(book_btn_id)
 
             # book appointment
             page.click(book_btn_id)
-        except playwright.helper.TimeoutError:
+        except PlaywrightTimeoutError:
             msg = "Failed to login. Check your password."
             print(msg)
             telegram_send.send(messages=[msg])
@@ -67,29 +66,31 @@ def main():
 
         change_btn_id = "#ctl00_PageRegion_MainContentRegion_ViewControl_spnReceiptAndBooking_BookingSummaryInfo_btnChangeBooking"
         try:
-            page.waitForSelector(change_btn_id, timeout=5000)
-        except playwright.helper.TimeoutError:
+            page.wait_for_selector(change_btn_id, timeout=5000)
+        except PlaywrightTimeoutError:
             print("No appointments to rebook.")
             return
 
         current_booking_id = "#ctl00_PageRegion_MainContentRegion_ViewControl_spnReceiptAndBooking_BookingSummaryInfo_lblDate"
-        current_booking = page.textContent(current_booking_id)
+        current_booking = page.text_content(current_booking_id)
         current_booking = dateparser.parse(current_booking)
-        page.click(change_btn_id)
-        page.waitForNavigation()
+        current_booking_date = datetime.date(current_booking.year, current_booking.month, current_booking.day)
+
+        with page.expect_navigation():
+            page.click(change_btn_id)
 
         # button to go to the next month
         next_btn_id = "#ctl00_BodyRegion_PageRegion_MainRegion_appointmentReservation_appointmentCalendar_btnNext"
 
         # initialize view_month to be just slightly less than the current booking
         view_month = current_booking - (current_booking - dateparser.parse("1 day"))
-
         # iterate over months trying to find available appointments
+        rebooking_possible = False
         while current_booking > view_month:
-            view_month_txt = page.querySelector("h2").innerText()
+            view_month_txt = page.query_selector("h2").inner_text()
             view_month = dateparser.parse(view_month_txt)
 
-            num_closed = len(page.querySelectorAll('css=[class="bookingCalendarClosedDay"]'))
+            num_closed = len(page.query_selector_all('css=[class="bookingCalendarClosedDay"]'))
             num_days_in_month = monthrange(view_month.year, view_month.month)[1]
             if num_days_in_month == num_closed:
                 print("Reached a fully closed month.")
@@ -97,20 +98,26 @@ def main():
 
             bookable = []
             for class_id in [".bookingCalendarHalfBookedDay", ".bookingCalendarBookableDay", ".bookingCalendarBookedDay"]:
-                bookable.extend(page.querySelectorAll(class_id))
+                bookable.extend(page.query_selector_all(class_id))
 
             if bookable:
-                bookable = sorted(bookable, key=lambda x: int(x.innerText().split()[0]))
-                bookable_day = int(bookable[0].innerText().split()[0])
-                if bookable_day < current_booking.day:
+                bookable = sorted(bookable, key=lambda x: int(x.inner_text().split()[0]))
+                bookable_day = int(bookable[0].inner_text().split()[0])
+                bookable_date = datetime.date(view_month.year, view_month.month, bookable_day)
+                if bookable_date < current_booking_date:
                     msg = f"It is possible to rebook the appointment on {bookable_day}, {view_month_txt}!"
                     send_success(page, msg)
+                    print(msg) # Also print message to stdout/log
+                    rebooking_possible = True
                     break
 
-            page.click(next_btn_id)
-            page.waitForNavigation()
+            with page.expect_navigation():
+                page.click(next_btn_id)
+            print(f"No possibilities to rebook for month {view_month_txt}.")
 
-        print("No possibilities to rebook.")
+        if not rebooking_possible:
+            print(f"No possibilities to rebook date earlier than {current_booking_date}.")
+
 
 
 if __name__ == "__main__":
